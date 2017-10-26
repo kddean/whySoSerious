56de7268f4caf2ac20681378037fe908c1217aa2
khadeidredean <khadeidredean@gmail.com>
2017-10-26 11:58:25 -0600

    Popups added. Base Bedroom dropped in.

---
+++ WhySoSerious/Assets/Scripts/Popup.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour {
+
+    public GameObject popup;
+    public GameObject ui;
+    public GameObject panel;
+    public GameObject text;
+    bool t;
+
+	// Use this for initialization
+	void Start () {
+       /* ui = GameObject.FindGameObjectWithTag("UI");
+        panel = GameObject.FindGameObjectWithTag("Panel");
+        BedPopup = GameObject.FindGameObjectWithTag("Popup");
+        */
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if (t == false)
+        {
+            t = true;
+            ui.SetActive(true);
+            //panel.SetActive(true);
+            popup.SetActive(true);
+            text.SetActive(true);
+        }
+        else
+        {
+            t = false;
+            ui.SetActive(false);
+            //panel.SetActive(false);
+            popup.SetActive(false);
+            text.SetActive(false);
+        }
+
+    }
+}
