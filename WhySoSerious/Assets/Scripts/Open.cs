2ba711250acdf6f11c38272ba076094375cf68b9
khadeidredean <khadeidredean@gmail.com>
2017-10-30 21:04:36 -0600

    Added Bathroom Scene

---
+++ WhySoSerious/Assets/Scripts/Open.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Open : MonoBehaviour {
+
+    bool open;
+    Vector3 orign;
+    GameObject[] boxes;
+    private GameObject box;
+
+    
+    // Use this for initialization
+    void Start () {
+        open = false;
+        orign = this.transform.position;
+        boxes = GameObject.FindGameObjectsWithTag("BBox");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if( open == false)
+        {
+            open = true;
+            this.transform.Translate(new Vector3((-0.13f), 0, 0));
+            for( int i = 0; i <4; i++)
+            {
+                box = boxes[i];
+                box.transform.Translate(new Vector3((-0.09f), 0, 0));
+            }
+        }
+        else
+        {
+            open = false;
+            this.transform.position = orign;
+            for (int i = 0; i < 4; i++)
+            {
+                box = boxes[i];
+                box.transform.Translate(new Vector3((0.09f), 0, 0));
+            }
+        }
+    }
+}
